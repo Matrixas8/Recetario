502efd1f8501c537b7d0ed50550ea982ced25b7e
Matrixas8 <mm3082426@gmail.com>
2025-01-30 21:36:40 -0300

    pequeños detalles

--- Recetario/src/servicios/receta.js
+++ Recetario/src/servicios/receta.js
@@ -3,10 +3,6 @@ const API_KEY = 'cbf63a3de3cc4a7592f5ab4b59511bca'
 export const getRecetas = async ({ search, filters }) => {
     const category = filters.category
     const diet = filters.diet
-
-    console.log('search:', search)
-    console.log('category:', category)
-    console.log('diet:', diet)
     
     try {
         const response = await fetch(
